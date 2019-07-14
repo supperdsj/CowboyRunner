4f5ccb0ecaead815cab63d3f475dc309e3162bd9
232171773@qq.com <667976dsa>
2019-07-14 22:33:47 +0800

    change User Animation with Obstacles

--- Assets/Scripts/ObstacleScript.cs
+++ Assets/Scripts/ObstacleScript.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class ObstacleScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class ObstacleScript : MonoBehaviour {
+    float speed = -3f;
+    Rigidbody2D myBody;
+
+    void Awake() {
+        myBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        myBody.velocity=new Vector2(speed,0);
     }
 }
---
+++ Assets/Scripts/PlayerAnimationScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationScript : MonoBehaviour {
+    Animator anim;
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D target) {
+        if (target.gameObject.tag == "Obstacle") {
+            anim.Play("Idle");
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D target) {
+        if (target.gameObject.tag == "Obstacle") {
+            anim.Play("Run");
+        }
+    }
+}
